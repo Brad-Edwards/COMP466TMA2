811d99576e81a005ac91594edcaa52c09be859b2
brad <j.bradley.edwards@gmail.com>
2021-08-03 21:53:42 +0000

    Add quiz grading and grade insert into database

--- part2/mysql/create_courses_table.sql
+++ part2/mysql/create_courses_table.sql
@@ -4,6 +4,7 @@ CREATE TABLE courses (
     course_code VARCHAR(20) NOT NULL,
     summary TEXT NOT NULL,
     introduction TEXT NOT NULL,
+    content MEDIUMTEXT NOT NULL,
     PRIMARY KEY (course_id),
     CONSTRAINT UNIQUE_courses_course_name_course_code UNIQUE (course_name, course_code)
       
